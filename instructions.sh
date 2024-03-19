2fa8574b358d83c4149fb6e21f9b9a1222d3829d
Oussama Djedidi <oussd@github.com>
2024-03-19 07:49:26 +0300

    preparation

---
+++ instructions.sh
@@ -0,0 +1,10 @@
+## Download
+docker build -t kaggle-downloader .
+
+docker run -v $(pwd)/data:/data -e KAGGLE_USERNAME=$KAGGLE_USERNAME -e KAGGLE_KEY=$KAGGLE_KEY kaggle-downloader
+
+git checkout -b train
+cp -r ../data ./data
+
+## Train
+sudo unzip ./data/100-bird-species.zip -d ./datadata/
