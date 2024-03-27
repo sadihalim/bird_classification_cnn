9dac1b5009cf535115765bb2119cc55028d69944
Oussama Djedidi <oussd@github.com>
2024-03-28 02:33:51 +0300

    update the tail

--- download_data.sh
+++ download_data.sh
@@ -9,4 +9,4 @@ chmod 600 /root/.kaggle/kaggle.json
 kaggle datasets download -d gpiosenka/100-bird-species -p /data
 
 # Keep the container running so that the data can be copied out
-tail -f /dev/null
+# tail -f /dev/null
